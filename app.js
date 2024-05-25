8372f9da4413e2396cf995cdda4e01165ee13b76
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-05-25 16:36:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,76 +5,86 @@ app.use(cors());
 app.use(express.json()); // Para parsear el body de las solicitudes
 
 let peliculas = [
-  {
-    id: 1,
-    titulo: "Batman: El Caballero de la Noche",
-    protagonista: "Bruce Wayne / Batman",
-    caracteristicas: "Un multimillonario que lucha contra el crimen...",
-    urlPelicula: "https://www.filmaffinity.com/es/film867354.html"
-  },
-  {
-    id: 2,
-    titulo: "Inception",
-    protagonista: "Dom Cobb",
-    caracteristicas: "Un ladrón que roba secretos corporativos a través del uso de la tecnología de sueños compartidos...",
-    urlPelicula: "https://www.filmaffinity.com/es/film123456.html"
-  },
-  {
-    id: 3,
-    titulo: "The Matrix",
-    protagonista: "Neo",
-    caracteristicas: "Un hacker que descubre que su realidad es una simulación creada por inteligencias artificiales...",
-    urlPelicula: "https://www.filmaffinity.com/es/film234567.html"
-  },
-  {
-    id: 4,
-    titulo: "The Shawshank Redemption",
-    protagonista: "Andy Dufresne",
-    caracteristicas: "Un banquero acusado de asesinato encuentra esperanza y redención dentro de la prisión...",
-    urlPelicula: "https://www.filmaffinity.com/es/film345678.html"
-  },
-  {
-    id: 5,
-    titulo: "Pulp Fiction",
-    protagonista: "Vincent Vega",
-    caracteristicas: "Varias historias de crimen se entrelazan en Los Ángeles...",
-    urlPelicula: "https://www.filmaffinity.com/es/film456789.html"
-  },
-  {
-    id: 6,
-    titulo: "The Godfather",
-    protagonista: "Michael Corleone",
-    caracteristicas: "El patriarca de una familia mafiosa pasa el control de su imperio a su hijo...",
-    urlPelicula: "https://www.filmaffinity.com/es/film567890.html"
-  },
-  {
-    id: 7,
-    titulo: "Fight Club",
-    protagonista: "The Narrator",
-    caracteristicas: "Un hombre aburrido de su vida forma un club de lucha secreto...",
-    urlPelicula: "https://www.filmaffinity.com/es/film678901.html"
-  },
-  {
-    id: 8,
-    titulo: "Forrest Gump",
-    protagonista: "Forrest Gump",
-    caracteristicas: "Un hombre con un bajo coeficiente intelectual ha tenido experiencias extraordinarias a lo largo de su vida...",
-    urlPelicula: "https://www.filmaffinity.com/es/film789012.html"
-  },
-  {
-    id: 9,
-    titulo: "Interstellar",
-    protagonista: "Cooper",
-    caracteristicas: "Un equipo de exploradores viaja a través de un agujero de gusano en el espacio en un intento por asegurar la supervivencia de la humanidad...",
-    urlPelicula: "https://www.filmaffinity.com/es/film890123.html"
-  },
-  {
-    id: 10,
-    titulo: "The Lord of the Rings: The Return of the King",
-    protagonista: "Frodo Baggins",
-    caracteristicas: "Un hobbit y sus amigos continúan su misión para destruir un anillo poderoso...",
-    urlPelicula: "https://www.filmaffinity.com/es/film901234.html"
-  }
+    {
+        "id": 1,
+        "titulo": "Batman: El Caballero de la Noche",
+        "protagonista": "Bruce Wayne / Batman",
+        "caracteristicas": "Un multimillonario que lucha contra el crimen...",
+        "urlPelicula": "https://www.filmaffinity.com/es/film867354.html",
+        "urlImagen": "https://pics.filmaffinity.com/the_dark_knight-102763119-large.jpg"
+      },
+      {
+        "id": 2,
+        "titulo": "Inception",
+        "protagonista": "Dom Cobb",
+        "caracteristicas": "Un ladrón que roba secretos corporativos a través del uso de la tecnología de sueños compartidos...",
+        "urlPelicula": "https://www.filmaffinity.com/es/film123456.html",
+        "urlImagen": "https://pics.filmaffinity.com/inception-652954101-large.jpg"
+      },
+      {
+        "id": 3,
+        "titulo": "The Matrix",
+        "protagonista": "Neo",
+        "caracteristicas": "Un hacker que descubre que su realidad es una simulación creada por inteligencias artificiales...",
+        "urlPelicula": "https://www.filmaffinity.com/es/film234567.html",
+        "urlImagen": "https://pics.filmaffinity.com/the_matrix-155050517-large.jpg"
+      },
+      {
+        "id": 4,
+        "titulo": "The Shawshank Redemption",
+        "protagonista": "Andy Dufresne",
+        "caracteristicas": "Un banquero acusado de asesinato encuentra esperanza y redención dentro de la prisión...",
+        "urlPelicula": "https://www.filmaffinity.com/es/film345678.html",
+        "urlImagen": "https://pics.filmaffinity.com/the_shawshank_redemption-576140557-large.jpg"
+      },
+      {
+        "id": 5,
+        "titulo": "Pulp Fiction",
+        "protagonista": "Vincent Vega",
+        "caracteristicas": "Varias historias de crimen se entrelazan en Los Ángeles...",
+        "urlPelicula": "https://www.filmaffinity.com/es/film456789.html",
+        "urlImagen": "https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg"
+      },
+      {
+        "id": 6,
+        "titulo": "The Godfather",
+        "protagonista": "Michael Corleone",
+        "caracteristicas": "El patriarca de una familia mafiosa pasa el control de su imperio a su hijo...",
+        "urlPelicula": "https://www.youtube.com/watch?v=UaVTIH8mujA",
+        "urlImagen": "https://pics.filmaffinity.com/the_godfather-488102675-large.jpg"
+      },
+      {
+        "id": 7,
+        "titulo": "Fight Club",
+        "protagonista": "The Narrator",
+        "caracteristicas": "Un hombre aburrido de su vida forma un club de lucha secreto...",
+        "urlPelicula": "https://www.youtube.com/watch?v=BdJKm16Co6M",
+        "urlImagen": "https://pics.filmaffinity.com/fight_club-320750671-large.jpg"
+      },
+      {
+        "id": 8,
+        "titulo": "Forrest Gump",
+        "protagonista": "Forrest Gump",
+        "caracteristicas": "Un hombre con un bajo coeficiente intelectual ha tenido experiencias extraordinarias a lo largo de su vida...",
+        "urlPelicula": "https://www.youtube.com/watch?v=bLvqoHBptjg",
+        "urlImagen": "https://pics.filmaffinity.com/forrest_gump-212765827-large.jpg"
+      },
+      {
+        "id": 9,
+        "titulo": "Interstellar",
+        "protagonista": "Cooper",
+        "caracteristicas": "Un equipo de exploradores viaja a través de un agujero de gusano en el espacio en un intento por asegurar la supervivencia de la humanidad...",
+        "urlPelicula": "https://www.youtube.com/watch?v=zSWdZVtXT7E",
+        "urlImagen": "https://pics.filmaffinity.com/interstellar-366875261-large.jpg"
+      },
+      {
+        "id": 10,
+        "titulo": "The Lord of the Rings: The Return of the King",
+        "protagonista": "Frodo Baggins",
+        "caracteristicas": "Un hobbit y sus amigos continúan su misión para destruir un anillo poderoso...",
+        "urlPelicula": "https://www.youtube.com/watch?v=r5X-hFf6Bwo",
+        "urlImagen": "https://pics.filmaffinity.com/the_lord_of_the_rings_the_return_of_the_king-178294596-large.jpg"
+      }
 ];
 
 // Obtener todas las películas
